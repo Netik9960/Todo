c5a28ca30b89c03f4e7bc886da994428b9338375
Netik9960 <73925093+Netik9960@users.noreply.github.com>
2024-06-20 16:21:34 +0530

    shadow

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 const TodoItem = ({ todo, toggleComplete, removeTodo }) => {
   return (
-    <div style={{ textDecoration: todo.completed ? 'line-through' : '' }}>
+    <div className='delete' style={{ textDecoration: todo.completed ? 'line-through' : '' }}>
       {todo.text}
       {/* <button onClick={() => toggleComplete(todo.id)}></button> */}
-      <button className='delete' onClick={() => removeTodo(todo.id)}>DEL</button>
+      <button onClick={() => removeTodo(todo.id)}>DEL</button>
     </div>
   );
 };
